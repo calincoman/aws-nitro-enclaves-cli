52ef603283b89c56dc6fb0e33e59fd2e2ceb3e09
Calin-Alexandru Coman <calincom@amazon.com>
2022-08-03 17:02:32 +0000

    calincoman::Add cache logic

--- enclave_build/src/cache_manager.rs
+++ enclave_build/src/cache_manager.rs
@@ -1,12 +1,21 @@
+// Copyright 2019-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::collections::{HashMap, hash_map::Entry};
 use std::fs::File;
 use std::io::Read;
+
+use oci_distribution::Reference;
+use oci_distribution::client::ImageData;
+
+use crate::utils::{ExtractLogic, Image};
 
 #[derive(Debug, PartialEq)]
 pub enum CacheError {
     FindImageError(String),
     ImageFileError(String),
     DataCacheError(String),
+    ArgumentError(String),
 
     CacheCreationError(String),
     UriNotFoundError(String),
@@ -29,17 +38,41 @@ impl CacheManager {
         }
     }
 
+    pub fn get_values(&self) -> &HashMap<String, String> {
+        &self.values
+    }
+
+    pub fn get_values_mut(&mut self) -> &mut HashMap<String, String> {
+        &mut self.values
+    }
+
     /// Return the image hash corresponding to the image URI if available, or else return an error
-    pub fn get_image_hash(&mut self, uri: &String) -> Result<String, CacheError> {
-        let image_hash = match self.values.entry(uri.to_string()) {
-            Entry::Occupied(val) => Some(val.get().clone()),
-            Entry::Vacant(_) => None
+    pub fn get_image_hash(&self, uri: &String) -> Result<String, CacheError> {
+        let image_hash: Option<&String> = match self.values.get(uri) {
+            Some(img_hash) => Some(img_hash),
+            None => None,
         };
 
         match image_hash {
-            Some(hash) => Ok(hash),
+            Some(hash) => Ok(hash.to_string()),
             None => Err(CacheError::UriNotFoundError("Image URI was not found. Image is probably not cached.".to_string()))
         }
+    }
+
+    /// Record the new image that was added to the cache
+    pub fn record_image(&mut self, image: &Image) -> Result<(), CacheError> {
+        // Get the image's hash (digest) and URI
+        let image_hash = match ExtractLogic::extract_image_hash(image.data()) {
+            Ok(aux) => aux,
+            Err(err) => {
+                return Err(CacheError::DataCacheError(format!("Failed to record image to hash: {:?}", err)));
+            }
+        };
+        let image_uri = image.reference().whole();
+
+        self.add_entry(&image_uri, &image_hash);
+
+        Ok(())
     }
 
     /// Add a new image URI <-> hash entry to the hashmap
@@ -47,7 +80,7 @@ impl CacheManager {
         self.values.insert(uri.to_string(), hash.to_string());
     }
 
-    /// Populate the hashmap with the values from the JSON index file which contains the mappings
+    /// Populate the hashmap with the values from a JSON index file which contains the mappings
     pub fn populate_hashmap(&mut self, index_file_path: &String) -> Result<(), CacheError> {
         let index_file = File::open(index_file_path);
         match &index_file {
--- enclave_build/src/constants.rs
+++ enclave_build/src/constants.rs
@@ -1,3 +1,6 @@
+// Copyright 2019-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 
 /// Docker inspect architecture constants
 pub const DOCKER_ARCH_ARM64: &str = "arm64";
@@ -10,3 +13,9 @@ pub const CACHE_ROOT_FOLDER: &str = "XDG_DATA_DIRS";
 
 /// The name of the actual image file from the image cache folder
 pub const IMAGE_FILE_NAME: &str = "image_file";
+
+/// Name of the cache file where 'ENV' expressions are stored
+pub const ENV_CACHE_FILE_NAME: &str = "env.sh";
+
+/// Name of the cache file where 'CMD' expressions are stored
+pub const CMD_CACHE_FILE_NAME: &str = "cmd.sh";
